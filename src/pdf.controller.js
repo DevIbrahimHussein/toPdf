290b170cefcf32a49bdaa33a0f386a990ce3fd97
Ibrahim Hussein <ibrahimhussein19.98@gmail.com>
2021-07-28 12:20:44 +0300

    Update & delete added

--- src/pdf.controller.js
+++ src/pdf.controller.js
@@ -1,5 +1,7 @@
 const base64 = require('base64topdf')
+const pdfModel = require('./pdf.model')
 const model = require('./pdf.model')
+const fs = require('fs')
 
 exports.exportToPdf = async(req, res) => {
 
@@ -24,5 +26,56 @@ exports.exportToPdf = async(req, res) => {
     } catch (e) {
         res.status(500).json({ msg: e })
     }
+
+}
+
+exports.updatePdf = async(req, res) => {
+
+    try {
+
+        let updatedBody = {}
+
+        // get current pdf
+        const currentPdf = await pdfModel.findById(req.params.pdfId)
+
+        if(req.body.pdf){
+            
+            let base64Str = ''
+            
+            for(var i = 0 ; i < req.body.pdf.length; i++){
+                base64Str += req.body.pdf[i].base64
+            }
+
+            fs.writeFile(`${currentPdf}.pdf`)
+
+        }
+
+        if(req.body.filename) {
+            updatedBody.name = req.body.filename
+            await pdfModel.findByIdAndUpdate(req.params.pdfId, updatedBody)
+            fs.rename(`${currentPdf.name}.pdf`, `${req.body.filename}.pdf`)
+        }
+
+        return res.json({ msg: 'success' })
+
+    } catch (e) {
+        res.status(500).json({ msg: e })
+    }
+
+}
+
+exports.deletePdf = async(req, res) => {
+
+    try {
+        const deletedPdf = await pdfModel.findByIdAndDelete(req.params.pdfId)
+    
+        fs.unlinkSync(`${deletedPdf.name}.pdf`)
+    
+        return res.json({ msg: 'Deleted Successfully' })
+
+    } catch (e){
+        return res.status(500).json({ msg: e })
+    }
+
 
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { exportToPdf } = require('./pdf.controller')
+const { exportToPdf, updatePdf, deletePdf } = require('./pdf.controller')
 const router = express.Router()
 
-router.post('/', 
-    exportToPdf
-)
+router.post('/', exportToPdf)
+
+router.put('/:pdfId', updatePdf)
+
+router.delete('/:pdfId', deletePdf)
 
 module.exports = router
